cd42a23a42fb44f026d02b9183535a77d7a2da95
shantanuB7 <shantanub@spanidea.com>
2019-02-20 12:07:32 +0530

    images folder added

--- script.js
+++ script.js
@@ -2524,3 +2524,34 @@ function handler() {
     return false;
 }
 */
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
+//102. 
+/*
+document.getElementById("contents").onclick = function(event) {
+    
+    function handleLink(href) {
+        
+        let isLeaving = confirm("Are you sure?")
+        
+        if(!isLeaving) return false;
+    }
+    
+    let target = event.target.closest('a')
+    
+    console.log(target)
+    
+    if(target && contents.contains(target)) {
+        
+        return handleLink(target.getAttribute('href'))
+    }
+}
+*/
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
+//103. 
+
