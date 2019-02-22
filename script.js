bc4c652c07cae8e059be016a24c5c5cc74537786
shantanuB7 <shantanub@spanidea.com>
2019-02-22 19:03:35 +0530

    22-2-2019

--- script.js
+++ script.js
@@ -1072,7 +1072,6 @@ edArr(arr)
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 
 //44. Sum input.
-
 /*
 function sumInput() {
     
@@ -2612,8 +2611,8 @@ function runOnKeys(func, ...codes) {
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 
-//106.Not getting o/p 
-
+//106.
+/*
 document.onmouseover = function(event) {
     
     
@@ -2627,6 +2626,15 @@ document.onmouseover = function(event) {
     tooltip = showTooltip(target, target.dataset.tooltip)
 //    console.log(tooltip)
 //        
+}
+
+
+document.onmouseout = function() {
+    
+    if(tooltip) {
+      tooltip.remove()
+      tooltip = false;
+    }
 }
 
 function showTooltip(target, data) {
@@ -2662,6 +2670,7 @@ function showTooltip(target, data) {
         return element;
     
     }
+*/
 
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------
@@ -2716,4 +2725,215 @@ thumb.onmousedown = function(event) {
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 
 //109.
+/*
+console.log(genres.value)
 
+let option = new Option("Classic","classic")
+
+genres.append(option)
+
+option.selected = true
+*/
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
+//110.
+/*
+let area = null
+
+let view = document.getElementById("view")
+
+view.onclick = function() {
+    editStart()
+}
+
+function editStart() {
+    area = document.createElement("textarea")
+    area.className = 'edit'
+    area.value = view.innerHTML;
+    
+    area.onkeydown = function(event) {
+        if(event.key == 'Enter') {
+            this.blur()
+        }
+    }
+    
+    area.onblur = function() {
+        editEnd();
+    }
+    
+    view.replaceWith(area)
+    area.focus();
+}
+
+function editEnd() {
+    view.innerHTML = area.value;
+    area.replaceWith(view)
+}
+*/
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
+//111.
+/*
+let editingTd;
+
+let table = document.getElementById('table')
+
+table.onclick = function(event) {
+    let target = event.target.closest('.edit-cancel,.edit-ok,td')
+    
+    if(!table.contains(target)) return;
+    
+    if(target.className == 'edit-cancel') {
+        
+        finishTdEdit(editingTd.elem, false)
+        
+    } else if(target.className == 'edit-ok') {
+        
+        finishTdEdit(editingTd.elem, true)
+        
+    } else if(target.nodeName == 'TD') {
+        
+        if(editingTd) return;
+        
+        makeTdEditable(target)
+    }
+}
+
+function makeTdEditable(td) {
+    editingTd = {
+        elem: td,
+        data: td.innerHTML
+    };
+    
+    td.classList.add('edit-td')
+    
+    let textarea = document.createElement('textarea')
+    textarea.style.width = td.clientWidth + 'px'
+    textarea.style.height = td.clientHeight + 'px'
+    textarea.className = 'edit-area';
+    
+    textarea.value = td.innerHTML;
+    td.innerHTML = '';
+    td.appendChild(textarea);
+    textarea.focus();
+    
+    td.insertAdjacentHTML("beforeEnd", '<div class = "edit-controls"><button class = "edit-ok">OK</button><button class = "edit-cancel">CANCEL</button></div>')
+}
+
+function finishTdEdit(td, isOk) {
+    if(isOk) {
+        td.innerHTML = td.firstChild.value;
+    } else {
+        td.innerHTML = editingTd.data
+    }
+    
+    td.classList.remove('edit-td');
+    editingTd = null
+}
+*/
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
+//112.
+/*
+deposit.oninput = calculate
+months.onchange = calculate
+
+interest.oninput = calculate
+
+function calculate() {
+    
+    let initial = document.getElementById('deposit').value
+    if(!initial) return;
+    
+    let interest = document.getElementById('interest').value * 0.01
+    if(!interest) return;
+    
+    let years = document.getElementById('months').value/12
+    
+    document.getElementById('result').innerHTML = Math.round(initial * (1 + interest*years))
+}
+*/
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
+//113. 
+
+function showCover() {
+      let coverDiv = document.createElement('div');
+      coverDiv.id = 'cover-div';
+
+      // make the page unscrollable while the modal form is open
+      document.body.style.overflowY = 'hidden';
+
+      document.body.append(coverDiv);
+    }
+
+    function hideCover() {
+      document.getElementById('cover-div').remove();
+      document.body.style.overflowY = '';
+    }
+
+    function showPrompt(text, callback) {
+      showCover();
+      let form = document.getElementById('prompt-form');
+      let container = document.getElementById('prompt-form-container');
+      document.getElementById('prompt-message').innerHTML = text;
+      form.text.value = '';
+
+      function complete(value) {
+        hideCover();
+        container.style.display = 'none';
+        document.onkeydown = null;
+        callback(value);
+      }
+
+      form.onsubmit = function() {
+        let value = form.text.value;
+        if (value == '') return false; // ignore empty submit
+
+        complete(value);
+        return false;
+      };
+
+      form.cancel.onclick = function() {
+        complete(null);
+      };
+
+      document.onkeydown = function(e) {
+        if (e.key == 'Escape') {
+          complete(null);
+        }
+      };
+
+      let lastElem = form.elements[form.elements.length - 1];
+      let firstElem = form.elements[0];
+
+      lastElem.onkeydown = function(e) {
+        if (e.key == 'Tab' && !e.shiftKey) {
+          firstElem.focus();
+          return false;
+        }
+      };
+
+      firstElem.onkeydown = function(e) {
+        if (e.key == 'Tab' && e.shiftKey) {
+          lastElem.focus();
+          return false;
+        }
+      };
+
+      container.style.display = 'block';
+      form.elements.text.focus();
+    }
+
+    document.getElementById('show-button').onclick = function() {
+      showPrompt("Enter something<br>...smart :)", function(value) {
+        alert("You entered: " + value);
+      });
+    };
