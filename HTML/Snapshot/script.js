32b1fde1bfa0aca621f0373eb3f3ebb55317bac5
shantanuB7 <shantanub@spanidea.com>
2019-03-18 16:12:09 +0530

    Snapshot complete

--- HTML/Snapshot/script.js
+++ HTML/Snapshot/script.js
@@ -1,10 +1,43 @@
-$('.owl-carousel').owlCarousel({
-    margin: 10,
-    loop: true,
-    autoWidth: true,
-    items: 4
-})
+$('.loop').owlCarousel({
+    center: true,
+    items:2,
+    loop:true,
+    margin:10,
+    responsive:{
+        600:{
+            items:4
+        }
+    }
+});
+$('.nonloop').owlCarousel({
+    center: true,
+    items:2,
+    loop:false,
+    margin:10,
+    responsive:{
+        600:{
+            items:4
+        }
+    }
+});
 
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel();
 });
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.querySelector(".go-top").style.display = "block";
+  } else {
+    document.querySelector(".go-top").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
