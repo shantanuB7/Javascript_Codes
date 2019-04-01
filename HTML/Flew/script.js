bdeaae4ae1a8f1c6f40ee168fe3fcc8672dadd77
shantanuB7 <shantanub@spanidea.com>
2019-04-01 10:36:08 +0530

    Flew

---
+++ HTML/Flew/script.js
@@ -0,0 +1,12 @@
+$(function(){
+            $("#nav").load("nav.html");
+            $("#features").load("features.html");
+            $("#project").load("projects.html");
+            $("#client").load("client.html");
+            $("#blogs").load("blog.html");
+            $("#updates").load("update.html");
+            $("#footer").load("footer.html");
+            $("#pleasure").load("pleasure.html");
+            $("#price").load("price.html");
+
+            });
