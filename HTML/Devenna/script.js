d6aa96baac1c046b7873681456a793862f6061bd
shantanuB7 <shantanub@spanidea.com>
2019-04-09 10:51:59 +0530

    All upto date.

--- HTML/Devenna/script.js
+++ HTML/Devenna/script.js
@@ -1,1 +1,7 @@
- 
+$(function(){
+            $("#nav").load("nav.html");
+            $("#sect-six").load("sect-six.html");
+            $("#foot").load("footer.html");
+            $("#breads").load("breadcrumb.html");
+            $("#width").load("width.html");
+            }); 
