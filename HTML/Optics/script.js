5b3a2c22136ee2f98235fae72133363d0f362fd5
shantanuB7 <shantanub@spanidea.com>
2019-04-17 19:20:25 +0530

    JS Assignmants

--- HTML/Optics/script.js
+++ HTML/Optics/script.js
@@ -2,8 +2,6 @@ window.addEventListener('scroll', function(e) {
     
     var nav = document.getElementById('nav')
     
-    console.log(document.body.scrollTop)
-    
     if(document.body.scrollTop > 50) {
         
         nav.classList.add('back');
